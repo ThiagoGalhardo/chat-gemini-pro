edb1a80894168c2c8835e462fffdd04376b4d72b
Thiago Galhardo <thiago.galhardo@hotmail.com>
2024-05-08 16:23:42 -0300

    Add error message if response return error

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="q-pa-md">
-    <h4 class="text-weight-light text-green-3 q-mb-md">Chat - Gemini Pro</h4>
-    <!-- <q-card class="q-pa-md bg-grey-9" flat dark> -->
+    <h4 class="text-weight-light text-green-3 q-mb-md">Chat - Gemini 1.5 Pro</h4>
     <q-scroll-area ref="scrollAreaRef" style="height: 500px; max-width: 100%;">
-      <q-chat-message v-for="(message, index) in  messages " :key="index" :name="message.sender"
-        :avatar="message.avatar" text-html :text="[marked(message.content)]" :sent="message.sent"
-        :bg-color="message.bgColor" text-color="white" />
+      <q-chat-message v-for="(message, index) in  messages" :key="index" :name="message.sender" :avatar="message.avatar"
+        text-html :text="[marked(message.content)]" :sent="message.sent" :bg-color="message.bgColor" text-color="white" />
+      <q-btn v-if="errorChat" icon="refresh" stack color="blue" class="q-my-md" @click="reloadChat">Novo chat</q-btn>
       <q-chat-message v-if="loading" name="Gemini" avatar="https://cdn.quasar.dev/img/avatar.png" bg-color="secondary">
         <q-spinner-dots size="2rem" /> </q-chat-message>
     </q-scroll-area>
@@ -15,23 +14,24 @@
         <q-btn round dense flat icon="send" @click="sendMessage" :disabled="!newMessage" />
       </template>
     </q-input>
-    <!-- </q-card> -->
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { marked } from 'marked'
 import { useGetGenerativeModelGP } from '../composables/useGetGenerativeModelGP.js'
-const messages = ref([])
 
+const messages = ref([])
 const newMessage = ref('')
 const loading = ref(false)
 const scrollAreaRef = ref(null)
+const errorChat = ref(false)
 
-import { marked } from 'marked'
-import MarkdownIt from 'markdown-it'
 
-const mkd = new MarkdownIt()
+const reloadChat = () => {
+  location.reload()
+}
 
 const sendMessage = async () => {
   if (!newMessage.value) return
@@ -65,6 +65,20 @@ const sendMessage = async () => {
     messages.value.push(botMessage)
   } catch (error) {
     console.log({ error })
+
+    if (error.status) {
+      const botMessage =
+      {
+        sender: 'Gemini',
+        content: "Ocorreu um erro e eu parei, vamos começar outro chat?",
+        sent: false,
+        bgColor: 'secondary',
+        avatar: 'https://cdn.quasar.dev/img/avatar.png',
+        error: true
+      }
+      messages.value.push(botMessage)
+      errorChat.value = true
+    }
   } finally {
     loading.value = false
     newMessage.value = ''
