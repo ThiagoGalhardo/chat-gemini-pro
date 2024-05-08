e0fbce47dd9006ddeed1b7beb5bc5901f5bc69d2
Thiago Galhardo <thiago.galhardo@hotmail.com>
2024-05-08 16:25:53 -0300

    Change to gemini 1.5 pro and add safetySettings

--- src/composables/useGetGenerativeModelGP.js
+++ src/composables/useGetGenerativeModelGP.js
@@ -1,16 +1,41 @@
-import { useGenAi } from './useGenAi.js'
+import {
+  useGenAi,
+  harmCategory,
+  harmBlockThreshold
+} from './useGenAi.js'
 
 let chat = null;
 
 export const useGetGenerativeModelGP = async (data) => {
-  const model = await useGenAi('gemini-pro')
+  const model = await useGenAi('gemini-1.5-pro-latest')
 
   if (chat == null) {
     chat = model.startChat({
       history: [],
       generationConfig: {
-        maxOutputTokens: 8000,
+        temperature: 1,
+        topK: 0,
+        topP: 0.95,
+        maxOutputTokens: 8192,
       },
+      safetySettings: [
+        {
+          category: harmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: harmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: harmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: harmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: harmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: harmBlockThreshold.BLOCK_NONE,
+        },
+        {
+          category: harmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: harmBlockThreshold.BLOCK_NONE,
+        },
+      ]
     })
   }
 
